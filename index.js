55622fa1bc466cf40ac4cf0171e9dad453679ccf
Great Solomon <greatsoma2019@gmail.com>
2022-06-16 10:20:32 +0100

    sub commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const copy4 = document.getElementById("copy4")
 const state = document.querySelector("#state")
 let isValid = false
 const myValue = document.querySelector("#number");
- const arr = [
+const arr = [
    "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x",
    "c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H",
    "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", ";", "/", "?", "!", "@", "#", "$",
@@ -40,7 +40,7 @@ const generate = () => {
      generated4.value= gen4;
   } else if (myValue.value == "") {
          state.textContent = "Please enter a valid number!";
-   } else {
+  } else {
     //  generated1.value = "invalid input";
     //  generated2.value= "invalid input";
     //  generated3.value = "invalid input";
